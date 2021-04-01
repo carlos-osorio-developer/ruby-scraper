73c11ca922b9c8bc16ba769f11c346fa5272581d
carlos-osorio-developer <osorio.uis@gmail.com>
2021-04-01 14:47:55 -0500

    Add #extractor method for the Classifier class

--- lib/logic.rb
+++ lib/logic.rb
@@ -26,6 +26,7 @@ class Scraper
     data_s = @content.body.partition("\"results\":[").last
     @companies = data_s.split("{\"")
     @companies.shift
+    @companies.freeze
   end
 end
 
@@ -37,6 +38,17 @@ class Classifier
     @raw_data = company
     @company_categories = %w[id name website_url category contact_email mailing_list irc_channel tag_line precis
                              description tags primary_open_source_license image_url image_bg_color gplus_url twitter_url blog_url application_instructions topic_tags technology_tags proposal_tags ideas_list contact_method program_year]
+  end
+
+  def extractor(category)
+    return 'Please enter a string' unless category.is_a? String
+
+    index = @company_categories.index(category)
+    keyword = "\"#{@company_categories[index]}\":"
+    keyword_index = @raw_data.index(keyword) + keyword.length
+    next_keyword = "\"#{@company_categories[index + 1]}\":"
+    next_keyword_index = @raw_data.index(next_keyword) - 2
+    @raw_data[keyword_index..next_keyword_index]
   end
 end
 # rubocop: enable Layout/LineLength
