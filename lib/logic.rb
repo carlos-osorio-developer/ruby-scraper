4e9d7b3023b9d7610385b8a77f036e4718730da5
carlos-osorio-developer <osorio.uis@gmail.com>
2021-04-01 16:02:54 -0500

    Add #dictionary method for the Classifier class

--- lib/logic.rb
+++ lib/logic.rb
@@ -2,20 +2,22 @@ require 'byebug'
 require 'net/http'
 require 'uri'
 
+# rubocop: disable Layout/LineLength
 class Scraper
   attr_reader :content, :companies
 
   def initialize(page)
     uri = URI.parse("https://summerofcode.withgoogle.com/api/program/current/organization/?page=#{page}&page_size=48")
     request = Net::HTTP::Get.new(uri)
-    request["Sec-Ch-Ua"] = "^\\^Google"
-    request["Accept"] = "application/json, text/plain, */*"
-    request["Referer"] = "https://summerofcode.withgoogle.com/organizations/"
-    request["X-Content-Type-Options"] = "nosniff"
-    request["Sec-Ch-Ua-Mobile"] = "?0"
-    request["User-Agent"] = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.114 Safari/537.36"
+    request['Sec-Ch-Ua'] = '^\\^Google'
+    request['Accept'] = 'application/json, text/plain, */*'
+    request['Referer'] = 'https://summerofcode.withgoogle.com/organizations/'
+    request['X-Content-Type-Options'] = 'nosniff'
+    request['Sec-Ch-Ua-Mobile'] = '?0'
+    request['User-Agent'] =
+      'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.114 Safari/537.36'
     req_options = {
-      use_ssl: uri.scheme == "https",
+      use_ssl: uri.scheme == 'https'
     }
     @content = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
       http.request(request)
@@ -23,8 +25,8 @@ class Scraper
   end
 
   def into_array
-    data_s = @content.body.partition("\"results\":[").last
-    @companies = data_s.split("{\"")
+    data_s = @content.body.partition('"results":[').last
+    @companies = data_s.split('{"')
     @companies.shift
     @companies.freeze
   end
@@ -49,6 +51,16 @@ class Classifier
     next_keyword = "\"#{@company_categories[index + 1]}\":"
     next_keyword_index = @raw_data.index(next_keyword) - 2
     @raw_data[keyword_index..next_keyword_index]
+  end
+
+  def dictionary(keywords)
+    return 'Please enter an array' unless keywords.is_a? Array
+
+    hash = {}
+    keywords.each do |keyword|
+      hash[keyword.to_sym] = extractor(keyword)
+    end
+    hash
   end
 end
 # rubocop: enable Layout/LineLength
